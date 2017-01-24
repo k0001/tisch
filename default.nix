ee01322bf76dde453c8df97d1d1df6c7faf07f95
Renzo Carbonara <gnuk0001@gmail.com>
2017-01-24 18:22:56 +0100

    bump nix

--- default.nix
+++ default.nix
@@ -2,32 +2,34 @@ let
 _pkgs0 = import ((import <nixpkgs> {}).fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  rev = "4aadb9beb345898c7f7b06090b163e7f3debea5a";
-  sha256 = "1dka2knhi8pfb83iwph7chldys1df95dc3z2v38cqzy4m06qjir9";
+  rev = "c78022aad18bfed0d13e0191f58045296288e79a";
+  sha256 = "1ngrbpks56kx58n3iwwf5jwlj6mdv16d0xnzrnb1hcgham2k2dbv";
 }) {};
 
 in
 { pkgs ? _pkgs0
-, compiler ? "ghc801"
+, compiler ? "ghc802"
 }:
 
 let
+  inherit (pkgs) stdenv;
   hs = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
-      mkDerivation = args:
-        super.mkDerivation (args // {
-          enableLibraryProfiling = true;
-          configureFlags = (args.configureFlags or []) ++ [
-            "--enable-optimization=2"
-            "--ghc-options=-fprof-auto-calls"
-            "--ghc-options=-fprof-auto-exported"
-            "--ghc-options=-fprof-auto-top"
-            "--ghc-options=-fprof-cafs"
-          ];
-        });
+      #mkDerivation = args:
+      #  super.mkDerivation (args // {
+      #    enableLibraryProfiling = true;
+      #    configureFlags = (args.configureFlags or []) ++ [
+      #      "--enable-optimization=2"
+      #      "--ghc-options=-fprof-auto-calls"
+      #      "--ghc-options=-fprof-auto-exported"
+      #      "--ghc-options=-fprof-auto-top"
+      #      "--ghc-options=-fprof-cafs"
+      #      (stdenv.lib.optionalString stdenv.isLinux
+      #        "--ghc-options=-split-sections")
+      #    ];
+      #  });
       tisch = pkgs.haskell.lib.overrideCabal (self.callPackage ./pkg.nix {}) (drv: {
-        enableExecutableProfiling = true;
-        enableSplitObjs = false;
+      #  enableExecutableProfiling = true;
       });
       opaleye = pkgs.haskell.lib.overrideCabal super.opaleye (drv: {
         src = pkgs.fetchFromGitHub {
@@ -35,7 +37,6 @@ let
           repo = "haskell-opaleye";
           rev = "2844e4a221d14cfb1047630b52ba1e9bf5992f5a";
           sha256 = "188c6g2l5dqn037vh1k8r96zwlljr2f6ikakipcxr08kcwb7awbm";
-
         };
       });
     };
