d8cb2ba4423d0ec0cf8c50d373af70dd07731be3
Renzo Carbonara <gnuk0001@gmail.com>
2017-06-18 20:51:59 +0200

    default.nix

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@ let
 _pkgs0 = import ((import <nixpkgs> {}).fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  rev = "c78022aad18bfed0d13e0191f58045296288e79a";
-  sha256 = "1ngrbpks56kx58n3iwwf5jwlj6mdv16d0xnzrnb1hcgham2k2dbv";
+  rev = "870cf05b6fc2c877d3b1cfa045b8b6d7129b2f42";
+  sha256 = "0b0q6aqar3xj2ac0x2prh6bmmxd1ndx85fw2bvbaak8sgbc4xf2z";
 }) {};
 
 in
@@ -15,30 +15,7 @@ let
   inherit (pkgs) stdenv;
   hs = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
-      #mkDerivation = args:
-      #  super.mkDerivation (args // {
-      #    enableLibraryProfiling = true;
-      #    configureFlags = (args.configureFlags or []) ++ [
-      #      "--enable-optimization=2"
-      #      "--ghc-options=-fprof-auto-calls"
-      #      "--ghc-options=-fprof-auto-exported"
-      #      "--ghc-options=-fprof-auto-top"
-      #      "--ghc-options=-fprof-cafs"
-      #      (stdenv.lib.optionalString stdenv.isLinux
-      #        "--ghc-options=-split-sections")
-      #    ];
-      #  });
-      tisch = pkgs.haskell.lib.overrideCabal (self.callPackage ./pkg.nix {}) (drv: {
-      #  enableExecutableProfiling = true;
-      });
-      opaleye = pkgs.haskell.lib.overrideCabal super.opaleye (drv: {
-        src = pkgs.fetchFromGitHub {
-          owner = "tomjaguarpaw";
-          repo = "haskell-opaleye";
-          rev = "2844e4a221d14cfb1047630b52ba1e9bf5992f5a";
-          sha256 = "188c6g2l5dqn037vh1k8r96zwlljr2f6ikakipcxr08kcwb7awbm";
-        };
-      });
+      tisch = self.callPackage ./pkg.nix {};
     };
   };
   drv = hs.tisch;
